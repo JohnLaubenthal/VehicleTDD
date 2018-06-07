0d26f4383aced8cdc0a98dbb9bc92ad84f88f0ef
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-06-07 08:12:39 -0400

    first commit

---
+++ VehicleTDD.Test/VehicleTDDTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTDD.Test
+{[TestFixture]
+    class VehicleTDDTests
+    {
+
+        [Test]
+        public void VehicleTDD_Ctor() {
+            var Vehicle = new Vehicle(" ");
+            var result = Vehicle.GetName;
+            Assert.That(result, Is.EqualTo(" "));
+        }
+
+    }//class end
+}//namespace end
